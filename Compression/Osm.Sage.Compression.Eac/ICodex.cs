37ae19c666aa1330e090a135ed3765dbab92ed00
Victor Matia <vmatir@outlook.com>
2025-08-13 03:27:39 +0100

    Added the codex interface
    Now it is possible to define EAC compression codex.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Compression/Osm.Sage.Compression.Eac/ICodex.cs
@@ -0,0 +1,11 @@
+namespace Osm.Sage.Compression.Eac;
+
+public interface ICodex
+{
+    CodexInformation About { get; }
+    bool IsValid { get; }
+
+    int ExtractSize(ReadOnlySpan<byte> compressedData);
+    int Encode(ReadOnlyMemory<byte> uncompressedData, Span<byte> compressedData);
+    int Decode(ReadOnlyMemory<byte> compressedData, Span<byte> uncompressedData);
+}
