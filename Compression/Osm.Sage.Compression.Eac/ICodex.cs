d112e7c7a256be80edf658cb9cbc8b8343857c28
Victor Matia <vmatir@outlook.com>
2025-08-13 03:31:17 +0100

    Fixed the interface
    Now the codex interface properly defines the `IsValid` member as a
    method.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Compression/Osm.Sage.Compression.Eac/ICodex.cs
+++ Compression/Osm.Sage.Compression.Eac/ICodex.cs
@@ -6,8 +6,8 @@ namespace Osm.Sage.Compression.Eac;
 public interface ICodex
 {
     CodexInformation About { get; }
-    bool IsValid { get; }
 
+    bool IsValid(ReadOnlySpan<byte> compressedData);
     int ExtractSize(ReadOnlySpan<byte> compressedData);
     int Encode(ReadOnlyMemory<byte> uncompressedData, Span<byte> compressedData);
     int Decode(ReadOnlyMemory<byte> compressedData, Span<byte> uncompressedData);
