11074faf96f94b4f34e41137fd85d8e5f9224e5a
Victor Matia <vmatir@outlook.com>
2025-08-19 18:29:25 +0100

    Added the file system
    Now it is possible to compres/decompress files.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Sources/Compression/Osm.Sage.Compression.Nox/Compressor.cs
+++ Sources/Compression/Osm.Sage.Compression.Nox/Compressor.cs
@@ -7,7 +7,14 @@ public static class Compressor
 {
     public static void CompressFile(string inputPath, string outputPath)
     {
-        throw new NotImplementedException();
+        using var input = File.OpenRead(inputPath);
+        using var binaryReader = new BinaryReader(input);
+        using var output = File.OpenWrite(outputPath);
+        using var binaryWriter = new BinaryWriter(output);
+
+        LightZhl.Compressor compressor = new();
+        var compressed = compressor.Compress(binaryReader.ReadBytes((int)input.Length));
+        binaryWriter.Write(compressed.ToArray());
     }
 
     public static Memory<byte> CompressMemory(ReadOnlyMemory<byte> input)
--- Sources/Compression/Osm.Sage.Compression.Nox/Decompressor.cs
+++ Sources/Compression/Osm.Sage.Compression.Nox/Decompressor.cs
@@ -7,7 +7,14 @@ public static class Decompressor
 {
     public static void DecompressFile(string inputPath, string outputPath)
     {
-        throw new NotImplementedException();
+        using var input = File.OpenRead(inputPath);
+        using var binaryReader = new BinaryReader(input);
+        using var output = File.OpenWrite(outputPath);
+        using var binaryWriter = new BinaryWriter(output);
+
+        LightZhl.Decompressor decompressor = new();
+        var decompressed = decompressor.Decompress(binaryReader.ReadBytes((int)input.Length));
+        binaryWriter.Write(decompressed.ToArray());
     }
 
     public static Memory<byte> DecompressMemory(ReadOnlyMemory<byte> input)
